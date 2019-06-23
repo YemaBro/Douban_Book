4e8a1a99f0d3c9947ba36b59203b67ac3353e124
YemaBro <yema2358@163.com>
2019-06-23 15:16:51 +0800

    update spider

--- Douban/spiders/douban.py
+++ Douban/spiders/douban.py
@@ -8,15 +8,13 @@ class DoubanSpider(RedisSpider):
     name = 'douban'
     allowed_domains = ['book.douban.com']
     base_url = 'https://book.douban.com/tag/{tag}'
-    # tag_list = ['小说', '历史', '外国文学', '中国文学', '心理学', '散文', '计算机', '编程', '艺术', '金融']
-    redis_key = 'douban:urls'
+    tag_list = ['小说', '历史', '外国文学', '中国文学', '心理学', '散文', '计算机', '编程', '艺术', '金融']
+    # redis_key = 'douban:urls'
 
-    # def start_requests(self):
-    #     yield scrapy.Request(url='http://proxy.abuyun.com/switch-ip')
-    #     for tag in self.tag_list:
-    #         book_list_url = self.base_url.format(tag=tag)
-    #         yield scrapy.Request(url=book_list_url, callback=self.parse, dont_filter=False)
-    # def make_request_from_data(self, data):
+    def start_requests(self):
+        for tag in self.tag_list:
+            book_list_url = self.base_url.format(tag=tag)
+            yield scrapy.Request(url=book_list_url, callback=self.parse, dont_filter=False)
 
     def parse(self, response):
 
